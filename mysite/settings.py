edbee2eac31e1a0a620929c4fb4042773be7c72d
Rimu <rimu.murakami.12@gmail.com>
2023-02-12 16:43:17 +0900

    SECRET_KEY changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7uwob733vmq*)eca&)1im1_jwxh=&xuv^s%#8fqlcf^yc&_+bw'
+# SECRET_KEY = 'django-insecure-7uwob733vmq*)eca&)1im1_jwxh=&xuv^s%#8fqlcf^yc&_+bw'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
